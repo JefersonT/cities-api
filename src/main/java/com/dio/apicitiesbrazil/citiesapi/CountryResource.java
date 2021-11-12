9530c881d2bfac2259850d3f53920e1b149036a1
JefersonT <jefersontorres.f@gmail.com>
2021-11-12 12:21:34 -0300

    Parte 8

--- src/main/java/com/dio/apicitiesbrazil/citiesapi/CountryResource.java
+++ src/main/java/com/dio/apicitiesbrazil/citiesapi/CountryResource.java
@@ -1,13 +1,18 @@
 package com.dio.apicitiesbrazil.citiesapi;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import com.dio.apicitiesbrazil.citiesapi.countries.Country;
 import com.dio.apicitiesbrazil.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/countries")
@@ -18,7 +23,19 @@ public class CountryResource {
 
 
     @GetMapping
-    public List<Country> countries(){
-        return repository.findAll();
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public  ResponseEntity getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }
